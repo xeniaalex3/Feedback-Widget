49308a603fc2093c51eceb22598d7316de122fb9
xenia <xenia_onescu_3@hotmail.com>
2023-03-20 19:10:39 +0100

    finished structur widget form

--- web/src/components/Widget/Widget.tsx
+++ web/src/components/Widget/Widget.tsx
@@ -1,12 +1,15 @@
 import { ChatTeardropDots } from '@phosphor-icons/react';
-import { Popover } from '@headlessui/react'
+import { Popover } from '@headlessui/react';
+import WidgetForm from '../WidgetForm/WidgetForm';
 
 function Widget(){
   return(
-    <Popover className='absolute bottom-4 right-5'>
-     <Popover.Panel>hello world</Popover.Panel> 
+    <Popover className='absolute bottom-4 md:bottom-10 right-5 flex flex-col items-end'>
+     <Popover.Panel>
+      <WidgetForm/>
+    </Popover.Panel> 
 
-    <Popover.Button className='bg-brand-500 rounded-full p-3 h-12 text-white flex items-center group'>
+    <Popover.Button className='bg-brand-500 rounded-full p-3 h-12 text-white flex items-center group outline-none'>
       <ChatTeardropDots className='w-6 h-6'/>
       <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'><span className='pl-1'>Feedback</span> </span>
     </Popover.Button>
--- web/src/components/WidgetForm/WidgetForm.tsx
+++ web/src/components/WidgetForm/WidgetForm.tsx
@@ -1,7 +1,7 @@
 function WidgetForm(){
   return (
-    <div>
-
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+hello world
     </div>
   )
 }
