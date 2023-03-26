746735b479521fc5d810aab82dbec64a49634ca6
xenia <xenia_onescu_3@hotmail.com>
2023-03-26 20:09:44 +0200

    styling button send feedback

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from 'phosphor-react';
+import { ArrowLeft, Camera } from 'phosphor-react';
 import CloseButton from "../../CloseButton/CloseButton";
 import { FeedbackType, feedbackTypes } from "../WidgetForm";
 
@@ -29,11 +29,27 @@ function FeedbackContentStep({feedbackType, onFeedbackRestartRequested}: Feedbac
 
  <form className='my-4 w-full'>
 <textarea 
-className="min-w-[302px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-500 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none"
+className="min-w-[302px] w-full min-h-[112px] text-sm placeholder-zinc-500 text-zinc-100 border-zinc-500 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
 placeholder='Tell us in detail what is happening...'
 >
-
 </textarea>
+
+<footer className='flex gap-2 mt-2'>
+
+  <button
+  type='button'
+  className='p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors'
+  >
+   <Camera className='w-6 h-6 text-zinc-400'/>
+  </button>
+
+  <button
+  type='submit'
+  className='p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:right-2 focus:ring-offset-2 transition-colors'
+  >
+Send feedback
+  </button>
+</footer>
  </form>
  </>
   )
