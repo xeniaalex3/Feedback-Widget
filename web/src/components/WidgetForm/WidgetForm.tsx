06995bc2be0bb5ff7ff08c39f922de8965f7b494
xenia <xenia_onescu_3@hotmail.com>
2023-03-26 15:45:09 +0200

    styling several box's

--- web/src/components/WidgetForm/WidgetForm.tsx
+++ web/src/components/WidgetForm/WidgetForm.tsx
@@ -1,8 +1,8 @@
-import CloseButton from "../CloseButton/CloseButton"
+import CloseButton from '../CloseButton/CloseButton'
 
-import bugImage from '../../assets/img/bug.svg';
-import ideaImage from '../../assets/img/idea.svg';
-import otherImage from '../../assets/img/thought.svg';
+import bugImage from '../../assets/img/bug.svg'
+import ideaImage from '../../assets/img/idea.svg'
+import otherImage from '../../assets/img/thought.svg'
 
 const feedbackTypes = {
   BUG: {
@@ -28,17 +28,27 @@ const feedbackTypes = {
   }
 }
 
-
 function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-
-      <header>
+      <header className='text-center'>
         <span className="text-xl leading-6">Leave your feedback</span>
-        <CloseButton/>
+        <CloseButton />
       </header>
       <div className="flex py-8 gap-2 w-full">
-
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button 
+            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus::outline-none" 
+            type='button' 
+            key={key}
+            
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          )
+        })}
       </div>
     </div>
   )
