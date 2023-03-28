c10baec490d99978bda980e8ac5c280cf6b516c7
xenia <xenia_onescu_3@hotmail.com>
2023-03-28 18:36:29 +0200

    testing sendMail

--- server/src/use-cases/submit-feedback-use-case.test.ts
+++ server/src/use-cases/submit-feedback-use-case.test.ts
@@ -1,8 +1,11 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
 
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
 const submitFeedback = new SubmitFeedbackUseCase(
-  { create: async () => {} },
-  { sendMail: async () => {} }
+  { create: createFeedbackSpy },
+  { sendMail: sendMailSpy }
 )
 
 describe('Submit feedback', () => {
@@ -12,6 +15,9 @@ describe('Submit feedback', () => {
       comment: 'example comment',
       screenshot: 'data:image/png;base64',
     })).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
   });
 
   it('should not be able to submit feedback without type', async () => { 
