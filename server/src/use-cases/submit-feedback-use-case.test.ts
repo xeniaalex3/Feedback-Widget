2187dc34aec493c6dec64d4d8f137701c32455c1
xenia <xenia_onescu_3@hotmail.com>
2023-03-28 18:24:37 +0200

    testing application, 100% functional

--- server/src/use-cases/submit-feedback-use-case.test.ts
+++ server/src/use-cases/submit-feedback-use-case.test.ts
@@ -1,1 +1,41 @@
-test('')
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
+
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: async () => {} },
+  { sendMail: async () => {} }
+)
+
+describe('Submit feedback', () => {
+  it('should be able to submit a feedback', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'example comment',
+      screenshot: 'data:image/png;base64',
+    })).resolves.not.toThrow();
+  });
+
+  it('should not be able to submit feedback without type', async () => { 
+    await expect(submitFeedback.execute({
+      type: '',
+      comment: 'example comment',
+      screenshot: 'data:image/png;base64',
+    })).rejects.toThrow();
+  });
+
+  it('should not be able to submit feedback without comment', async () => { 
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: '',
+      screenshot: 'data:image/png;base64',
+    })).rejects.toThrow();
+  });
+
+  it('should not be able to submit feedback with invalid screenshot', async () => { 
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'example comment',
+      screenshot: 'test.png',
+    })).rejects.toThrow();
+  });
+
+});
