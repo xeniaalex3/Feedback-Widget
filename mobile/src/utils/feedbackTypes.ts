558b6aec0d13ad91f5eda688ea944d68e491a5b3
xenia <xenia_onescu_3@hotmail.com>
2023-04-04 11:10:28 +0200

    install depend

--- mobile/src/utils/feedbackTypes.ts
+++ mobile/src/utils/feedbackTypes.ts
@@ -1,14 +1,14 @@
 export const feedbackTypes = {
   'BUG': {
-    title: 'Problema',
+    title: 'Issue',
     image: require('../assets/bug.png')
   },
   'IDEA': {
-    title: 'Ideia',
+    title: 'Idea',
     image: require('../assets/idea.png'),
   },
   'OTHER': {
-    title: 'Outro',
+    title: 'Other',
     image: require('../assets/thought.png'),
   },
 };
